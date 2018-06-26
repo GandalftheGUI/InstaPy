de684cc7520980e6f43365d5a6918ebcf3163a9e
Ian Remillard <Doom-Mac@Ians-iMac.local>
2018-06-26 16:08:11 -0700

    fire and forget

--- fire_and_forget_example.py
+++ fire_and_forget_example.py
@@ -2,6 +2,7 @@ from instapy import InstaPy
 import random
 import sys, os
 import getpass
+import time
 
 possible_tags = ['morainelake', 'instautah', 'agameoftones', 'moodygrams', 'mg5k', 'beautifuldestinations', 'explorecanada', 'tourcanada', 'canada',
 'banffnationalpark', 'banff', 'canadasworld', 'hikingadventures', 'discoverbanff', 'travelcanada', 'travelphotography', 'mybanff', 'canada', 'travel',
@@ -54,33 +55,45 @@ possible_tags = ['morainelake', 'instautah', 'agameoftones', 'moodygrams', 'mg5k
 'EarthVisuals',
 'welivetoexplore']
 
+
+import datetime
+
+def time_until_next_day(offset=0):
+    """
+    Get timedelta until end of day on the datetime passed, or current time.
+    """
+    dt = datetime.datetime.now()
+    tomorrow = dt + datetime.timedelta(days=1)
+    return datetime.datetime.combine(tomorrow, datetime.time.min) - dt + datetime.timedelta(hours=offset)
+
 try:
   print("Please enter your Instagram credentials")
   username_input = raw_input("Username: ")
-
   passowrd_input = getpass.getpass('Password: ')
 
-  session = InstaPy(username=username_input, password=passowrd_input)
-  session.max_likes = random.randint(700,950)
-  print "Max likes: {}".format(session.max_likes)
-  session.login()
 
-  #image_url = raw_input("Enter the image url would you like to use: ")
-  # session.like_from_image(url=image_url, amount=100)
+  while True:
+    session = InstaPy(username=username_input, password=passowrd_input)
+    session.max_likes = random.randint(700,950)
+    print "Max likes: {}".format(session.max_likes)
+    session.login()
+    tags = random.sample(possible_tags, len(possible_tags))
 
-  #session.like_by_tags(['utahgram', 'instautah', 'igutah', 'iloveutah', 'mountainlife', 'wowutah', 'rockymountains', 'adventureculture', 'optoutside', 'getoutdoors', 'hiking', 'hikeutah', 'utahisrad', 'adventureon', 'utahrocks', 'visitutah', 'exploreutah', 'iloveutah', 'beautahful', 'southernutah', 'utah'], amount=100)
+    #session.like_by_feed(amount=10, randomize=True, unfollow=True, interact=True)
+    session.like_by_tags(tags, amount=100)
+    session.end()
 
+    offset = time_until_next_day(8).total_seconds() + random.randint(0, 60*5)
+    start_at = datetime.datetime.now() + datetime.timedelta(seconds=offset)
 
-  tags = random.sample(possible_tags, len(possible_tags))
-
-  #session.like_by_feed(amount=10, randomize=True, unfollow=True, interact=True)
-  session.like_by_tags(tags, amount=100)
-
+    print "All done for the day!!"
+    print start_at.strftime('Sleeping until - %D %T')
+    time.sleep(offset)
 except:
     raise
 
 finally:
   """Ending the script"""
   # clears all the cookies, deleting you password and all information from this session
-  session.end()
+  #session.end()
 
